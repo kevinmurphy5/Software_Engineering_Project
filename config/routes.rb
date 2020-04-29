be0f35447b666bb0d49ad333c8b2c0110c3062d4
John Salajka <jsalajka@memphis.edu>
2020-04-28 23:09:01 -0500

    Requires validation to view pages that are not the welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'static_pages/welcome'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: redirect('/welcome', status: 302)
   get 'welcome', to: 'static_pages#welcome', as: 'welcome'
-
-
 end
