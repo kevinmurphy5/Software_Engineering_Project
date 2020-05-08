b11da38ef4ac0cc09ce98bc1a27e449731006c57
John Salajka <jsalajka@memphis.edu>
2020-05-08 00:11:44 -0500

    US4: users can now see a list of their bids

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,2 +1,11 @@
 class BidsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        bids = current_user.bids
+        auctions = Auction.all
+        respond_to do |format|
+            format.html {render :index, locals: {bids: bids, auctions: auctions}}
+        end
+    end
 end
