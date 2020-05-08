4c2e91d22365d5e31e7e0b1fb1d706680426832f
John Salajka <jsalajka@memphis.edu>
2020-05-07 22:43:25 -0500

    US8: can now create new auctions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/welcome'
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: redirect('/welcome', status: 302)
-  get 'welcome', to: 'static_pages#welcome', as: 'welcome'
-  get 'index', to: 'static_pages#index', as: 'index'
+	get 'static_pages/welcome'
+	devise_for :users
+	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+	root to: redirect('/welcome', status: 302)
+	get 'welcome',	to: 'static_pages#welcome', as: 'welcome'
+	get 'index',	to: 'static_pages#index',	as: 'index'
 
-  get 'auctions', to:'auctions#index', as: 'auctions'
-  get 'auctions/:id', to:'auctions#details', as: 'auction'
+	get	'auctions',		to:'auctions#index',	as: 'auctions'
+	get 'auctions/new', to:'auctions#new',		as: 'new_auction'
+	post 'auctions',	to:'auctions#create'
+	get 'auctions/:id', to:'auctions#details',	as: 'auction'
 end
